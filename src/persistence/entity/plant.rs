de2270ba097e672497489f5b6111cf60fd23e11c
aggalex <ubuntu1aggelalex@gmail.com>
2024-01-19 17:36:18 +0200

    Create plant & plant_profile tables

--- src/persistence/entity/plant.rs
+++ src/persistence/entity/plant.rs
@@ -1,15 +1,57 @@
-use crate::persistence::Transaction;
+use chrono::{DateTime, Utc};
+use postgres_from_row::FromRow;
+use uuid::Uuid;
+use crate::persistence::{FromRowExtension, QueryResult, Transaction};
+use crate::persistence::entity::user::FullUserInfoDto;
 
-#[derive(Clone)]
-pub struct Plant {
-    id: u64,
+#[derive(Clone, FromRow)]
+pub struct PlantDto {
+    pub id: i32,
     pub name: String,
-    pub plant_profile_id: u64,
+    pub profile_id: i32,
+    pub last_watered: Option<DateTime<Utc>>,
+    pub uuid: String,
     pub soil_moisture: f32,
-    pub last_watered: f32,
-    pub environment_humidity: f32,
-    pub environment_temperature: f32
+}
+
+#[derive(Clone, FromRow)]
+pub struct NewPlantDto {
+    pub name: String,
+    pub profile_id: i32,
 }
 
 #[derive(From, Clone)]
 pub struct PlantDao<'r>(&'r Transaction<'r>);
+
+impl PlantDao<'_> {
+    pub async fn find_by_id(&self, id: i32) -> QueryResult<Option<PlantDto>> {
+        self.0.query_opt(r#"SELECT * FROM plant WHERE id = $1"#, &[&id])
+            .await.map_err(Into::into).and_then(PlantDto::try_from_opt_row)
+    }
+
+    pub async fn update_moisture(&self, id: i32, moisture: f32) -> QueryResult<Option<PlantDto>> {
+        self.0.query_opt(r#"UPDATE plant SET soil_moisture = $2 WHERE id = $1 RETURNING *"#, &[&id, &moisture])
+            .await.map_err(Into::into).and_then(PlantDto::try_from_opt_row)
+    }
+
+    pub async fn create(&self, plant_dto: NewPlantDto) -> QueryResult<PlantDto> {
+        self.0.query_one(r#"INSERT INTO plant("name", profile_id, uuid) VALUES ($1, $2, $3) RETURNING *"#, &[
+            &plant_dto.name,
+            &plant_dto.profile_id,
+            &Uuid::new_v4().to_string()
+        ]).await.map_err(Into::into).and_then(PlantDto::try_from_row_owned)
+    }
+
+    pub async fn update(&self, id: i32, new_plant_dto: NewPlantDto) -> QueryResult<Option<PlantDto>> {
+        self.0.query_opt(r#"UPDATE plant SET name = $2, profile_id = $3 WHERE id = $1 RETURNING *"#, &[
+            &id,
+            &new_plant_dto.name,
+            &new_plant_dto.profile_id
+        ]).await.map_err(Into::into).and_then(PlantDto::try_from_opt_row)
+    }
+
+    pub async fn delete(&self, id: i32) -> QueryResult<()> {
+        self.0.query(r#"DELETE FROM plant WHERE id = $1"#, &[&id])
+            .await.map_err(Into::into).map(|_| ())
+    }
+}
---
+++ src/persistence/entity/plant_profile.rs
@@ -0,0 +1,52 @@
+use deadpool_postgres::GenericClient;
+use postgres_from_row::FromRow;
+use rocket::serde::{Deserialize, Serialize};
+use crate::persistence::entity::plant::PlantDto;
+use crate::persistence::{FromRowExtension, QueryResult, Transaction};
+
+#[derive(Clone, FromRow, Serialize)]
+pub struct PlantProfileDto {
+    pub id: i32,
+    pub name: String,
+    pub max_moisture: f32,
+    pub min_moisture: f32
+}
+
+#[derive(Clone, Deserialize)]
+pub struct NewPlantProfileDto {
+    pub name: String,
+    pub max_moisture: f32,
+    pub min_moisture: f32
+}
+
+#[derive(From, Clone)]
+pub struct PlantProfileDao<'r>(&'r Transaction<'r>);
+
+impl PlantProfileDao<'_> {
+    pub async fn find_by_id(&self, id: i32) -> QueryResult<Option<PlantProfileDto>> {
+        self.0.query_opt(r#"SELECT * FROM plant_profile WHERE id = $1"#, &[&id])
+            .await.map_err(Into::into).and_then(PlantProfileDto::try_from_opt_row)
+    }
+
+    pub async fn create(&self, new_plant_profile_dto: NewPlantProfileDto) -> QueryResult<PlantProfileDto> {
+        self.0.query_one(r#"INSERT INTO plant_profile (name, max_moisture, min_moisture) VALUES ($1, $2, $3)"#, &[
+            &new_plant_profile_dto.name,
+            &new_plant_profile_dto.min_moisture,
+            &new_plant_profile_dto.max_moisture
+        ]).await.map_err(Into::into).and_then(PlantProfileDto::try_from_row_owned)
+    }
+
+    pub async fn update(&self, id: i32, new_plant_profile_dto: NewPlantProfileDto) -> QueryResult<PlantProfileDto> {
+        self.0.query_one(r#"UPDATE plant_profile SET name = $2, max_moisture = $3, min_moisture = $4 WHERE id = $1"#, &[
+            &id,
+            &new_plant_profile_dto.name,
+            &new_plant_profile_dto.min_moisture,
+            &new_plant_profile_dto.max_moisture
+        ]).await.map_err(Into::into).and_then(PlantProfileDto::try_from_row_owned)
+    }
+
+    pub async fn delete(&self, id: i32) -> QueryResult<()> {
+        self.0.query_one(r#"DELETE from plant_profile WHERE id = $1"#, &[&id])
+            .await.map_err(Into::into).map(|_| ())
+    }
+}
